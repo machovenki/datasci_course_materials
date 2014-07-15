32207008e486f717d3294f8c6940c766ad898df0
VM023561 <venkatesh.muthusamy@cerner.com>
2014-07-15 10:32:58 -0500

    Fix all indentation

--- assignment1/frequency.py
+++ assignment1/frequency.py
@@ -1,35 +1,33 @@
 import sys
 import json
 import re
+from collections import Counter
+
 
 def lines(fp):
     print str(len(fp.readlines()))
 
+
 def main():
-	tweet_file = open(sys.argv[1])
-	word_frequency_count = {}
-
-	for line in tweet_file:
-		raw_data = json.loads(line)
-		#print raw_data
-		text = raw_data.get('text', "").lower().encode('utf-8')
-		new_text=re.findall(r"[\w']+", text)
-		print new_text
-		#print text
-        #for word in text:
-        #	if not word.isalpha():
-        #		print "BlahKey:"+word
-        #		if word in word_frequency_count:
-	    #    		oldcount = word_frequency_count[word]
-	    #    		word_frequency_count[word] = ++oldcount
-        	
-    	#		else:
-        #			word_frequency_count[word] = 1
-
+    tweet_file = open(sys.argv[1])
+    cnt = Counter()
+    for line in tweet_file:
+        raw_data = json.loads(line)
+        text = raw_data.get('text', "").lower().encode('utf-8')
+        new_text=re.findall(r"[\w']+", text)
         for word in new_text:
-        	word_frequency_count[word] = 'Test'
+            cnt[word] += 1
 
-	print word_frequency_count.items()
+    total_word_count =  float(sum(cnt.values()))
+
+    for k,v in cnt.items():
+        old_value = cnt[k]
+        cnt[k] =  (old_value/total_word_count)
+        if k.isalpha():
+            print k,cnt[k]
+
+
+
 
 
 if __name__ == '__main__':
